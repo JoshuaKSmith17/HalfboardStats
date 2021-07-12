af65b8379cb49d34ae4f52898081a577e543480e
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-12 18:49:14 -0500

    Added Standings ORM class.  This class serves as the top level container for NHL standings.

---
+++ Model/ObjectRelationalMappers/Standings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Model.ObjectRelationalMappers
+{
+    public class Standings
+    {
+        public string Copyright { get; set; }
+        public List<DivisionStandings> Records { get; set; }
+    }
+}
