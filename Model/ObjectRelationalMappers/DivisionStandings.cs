fb6b9961049e5e9148e5fbfd1a5d5262c3a649a2
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-12 18:47:55 -0500

    Added Division Standings Class.

---
+++ Model/ObjectRelationalMappers/DivisionStandings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Model.ObjectRelationalMappers
+{
+    public class DivisionStandings
+    {
+        public string StandingsType { get; set; }
+        public League League { get; set; }
+        public Division Division { get; set; }
+        public Conference Conference { get; set; }
+        public List<TeamRecords> TeamRecords { get; set; }
+    }
+}
