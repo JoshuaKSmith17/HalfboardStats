1d865c2178fb03554ced095954d0daa1da22b183
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-14 17:18:53 -0500

    Revert "0006 basic backend for get requests & object json mapping"

--- Model/ObjectRelationalMappers/Team.cs
+++ Model/ObjectRelationalMappers/Team.cs
@@ -9,7 +9,7 @@ namespace HalfboardStats.Model.ObjectRelationalMappers
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Link { get; set; }
+        public string City { get; set; }
 
     }
 }
