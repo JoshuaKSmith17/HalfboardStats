7901276b31bf50cdc2c72bfd42ed2866cf8e3fbd
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-19 18:49:30 -0500

    Added code to map a team Id to the team.

--- Model/Builders/StandingsBuilder.cs
+++ Model/Builders/StandingsBuilder.cs
@@ -38,6 +38,7 @@ namespace HalfboardStats.Model.Builders
                 for (int j = 0; j < mapper.Records[i].TeamRecords.Count; j++)
                 {
                     TeamRecord record = new TeamRecord();
+                    record.Id = mapper.Records[i].TeamRecords[j].Team.Id;
                     record.TeamName = mapper.Records[i].TeamRecords[j].Team.Name;
                     record.Conference = mapper.Records[i].Conference.Name;
                     record.Division = mapper.Records[i].Division.Name;
