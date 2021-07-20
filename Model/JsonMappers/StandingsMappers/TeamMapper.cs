ff813fdd4c4b1a6f3679094220f5c8d7a568fbb3
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-20 18:11:17 -0500

    Expanded TeamMapper.cs to capture all data from the Json file.

--- Model/JsonMappers/StandingsMappers/TeamMapper.cs
+++ Model/JsonMappers/StandingsMappers/TeamMapper.cs
@@ -10,6 +10,19 @@ namespace HalfboardStats.Model.JsonMappers
         public int Id { get; set; }
         public string Name { get; set; }
         public string Link { get; set; }
+        public VenueMapper Venue { get; set; }
+        public string Abbreviation { get; set; }
+        public string TeamName { get; set; }
+        public string LocationName { get; set; }
+        public int FirstYearOfPlay { get; set; }
+        public DivisionMapper Division { get; set; }
+        public ConferenceMapper Conference { get; set; }
+        public FranchiseMapper Franchise { get; set; }
+        public string ShortName { get; set; }
+        public string OfficialSiteUrl { get; set; }
+        public int FranchiseId { get; set; }
+        public bool Active { get; set; }
+
 
     }
 }
