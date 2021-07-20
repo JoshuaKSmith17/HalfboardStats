4b7b569c856e387778b83c75225da531a4b9f57b
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-19 20:52:02 -0500

    Added new interfaces for services registration purposes. (+1 squashed commits)
    Squashed commits:
    
    [89b7516] Created the ITeamRecord interface.

---
+++ Model/ObjectRelationalMappers/OrmInterfaces/IStandings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Model.ObjectRelationalMappers
+{
+    public interface IStandings
+    {
+        public List<TeamRecord> TeamRecords { get; set; }
+    }
+}
