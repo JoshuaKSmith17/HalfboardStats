2db23408e0b42b6e5660531a529939044e91cee8
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-21 18:53:51 -0500

    Created ORM classes that largely mimic their JSON mappers.

---
+++ Model/ObjectRelationalMappers/PlayerSeason.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Model.ObjectRelationalMappers
+{
+    public class PlayerSeason
+    {
+    }
+}
