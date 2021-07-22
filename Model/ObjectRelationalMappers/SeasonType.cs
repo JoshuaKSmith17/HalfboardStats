a717a0f10ea9e59545e609490de3ebef6aa6af8d
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-22 18:31:54 -0500

    Created seasonType enum for different split types the NHL offers.

---
+++ Model/ObjectRelationalMappers/SeasonType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Model.ObjectRelationalMappers
+{
+    public enum SeasonType
+    {
+        RegularSeason,
+        Playoffs
+    }
+}
