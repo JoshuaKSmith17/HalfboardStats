093b38b07a0076cbf2db4dacad100cf02b8cb69b
JoshuakSmith17 <joshuaksmith17@gmail.com>
2021-07-22 18:31:22 -0500

    Created new mapper classes to support the expanded TeamMapper class (+1 squashed commits)
    Squashed commits:
    
    [5bc4095] Created a seasonTypemapper.

---
+++ Model/JsonMappers/PlayerMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Model.JsonMappers
+{
+    public class PlayerMapper
+    {
+        public PersonMapper Person { get; set; }
+        public int JerseyNumber { get; set; }
+        public PositionMapper Position { get; set; }
+    }
+}
---
+++ Model/JsonMappers/TeamRosterMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Model.JsonMappers
+{
+    public class TeamRosterMapper
+    {
+        public string Copyright { get; set; }
+        public List<PlayerMapper> Roster { get; set; }
+    }
+}
