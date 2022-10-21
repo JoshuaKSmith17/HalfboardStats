5d57abefd4bbd2d8063e521e2a9284af1767e206
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-10-21 14:34:57 -0500

    Added functionality to take the JSON to Object mapped information and push it to the local database.

---
+++ HalfboardStats/Model/LocalRepositories/TeamLocalRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HalfboardStats.Model.ObjectRelationalMappers;
+using HalfboardStats.Model.Builders;
+
+namespace HalfboardStats.Model.LocalRepositories
+{
+    public class TeamLocalRepository : ITeamLocalRepository
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+        public List<Team> Teams { get; set; }
+        
+        public TeamLocalRepository(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public async void CreateTeams(HalfboardContext context)
+        {
+            var builder = (ITeamBuilder)ServiceProvider.GetService(typeof(ITeamBuilder));
+            Teams = await builder.BuildTeams();
+
+            foreach (var team in Teams)
+            {
+                var dbTeam = context.Teams.Find(team.TeamId);
+                if (dbTeam == null)
+                {
+                    context.Teams.Add(team);
+                }
+                else
+                {
+                    context.Entry(dbTeam).CurrentValues.SetValues(team);
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+}
