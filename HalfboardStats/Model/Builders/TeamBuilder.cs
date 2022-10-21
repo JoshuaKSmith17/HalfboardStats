ff1603e30093cd538da8b19b2ce38eb9a79949b8
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-10-21 14:34:13 -0500

    Added files to build JSON to Object mappers for the teams.

---
+++ HalfboardStats/Model/Builders/TeamBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HalfboardStats.Model.JsonMappers;
+using HalfboardStats.Model.ObjectRelationalMappers;
+using HalfboardStats.Model.Repositories;
+
+namespace HalfboardStats.Model.Builders
+{
+    public class TeamBuilder : ITeamBuilder
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public TeamBuilder(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public async Task<List<Team>> BuildTeams()
+        {
+            var repo = (ITeamRepository)ServiceProvider.GetService(typeof(ITeamRepository));
+            List<Team> teams = new List<Team>();
+
+            List<TeamMapper> teamMapper = await repo.GetTeams();
+
+            foreach (var t in teamMapper)
+            {
+                var team = new Team();
+
+                team.TeamId = t.Id;
+                team.DivisionId = t.Division.Id;
+                team.FranchiseId = t.FranchiseId;
+                team.Name = t.Name;
+                team.Link = t.Link;
+                team.Abbreviation = t.Abbreviation;
+                team.TeamName = t.TeamName;
+                team.LocationName = t.LocationName;
+                team.FirstYearOfPlay = t.FirstYearOfPlay;
+                team.ShortName = t.ShortName;
+                team.OfficialSiteUrl = t.OfficialSiteUrl;
+                team.IsActive = t.Active;
+
+                teams.Add(team);
+
+            }
+            return teams;
+        }
+    }
+}
