f040a968209f5f57ae1d40d23a661d4014b2559b
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-10-21 14:32:38 -0500

    Added files used to request team data from the NHL API.

---
+++ HalfboardStats/Model/Repositories/ITeamRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HalfboardStats.Model.JsonMappers;
+
+namespace HalfboardStats.Model.Repositories
+{
+    interface ITeamRepository
+    {
+        public Task<List<TeamMapper>> GetTeams();
+    }
+}
