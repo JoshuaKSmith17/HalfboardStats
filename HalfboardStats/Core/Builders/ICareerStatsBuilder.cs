575415bc17698578660509015b9a26756566208d
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-10-31 21:27:58 -0500

    Implemented class that handles converting JSON mapped data to ORM class data.

---
+++ HalfboardStats/Core/Builders/ICareerStatsBuilder.cs
@@ -0,0 +1,15 @@
+﻿using HalfboardStats.Core.JsonMappers.StatsMappers;
+using HalfboardStats.Core.ObjectRelationalMappers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Core.Builders
+{
+    public interface ICareerStatsBuilder
+    {
+        IServiceProvider ServiceProvider { get; set; }
+
+        List<RegularSeasonStats> BuildCareerStatsAsync(int Id, YearByYearMapper playerStats);
+    }
+}
