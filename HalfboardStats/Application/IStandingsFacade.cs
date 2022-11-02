cf920459ffe9a163854fbeca075c75025fee8ce0
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-11-01 20:50:24 -0500

    Created standings facade that serves as the gateway for the UI.

---
+++ HalfboardStats/Application/IStandingsFacade.cs
@@ -0,0 +1,14 @@
+﻿using HalfboardStats.Core.Builders;
+using HalfboardStats.Core.ObjectRelationalMappers.OrmInterfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Application
+{
+    public interface IStandingsFacade
+    {
+        IStandingsBuilder StandingsBuilder { get; set; }
+
+        Task<IDictionary<string, IEnumerable<ITeamRecord>>> GetStandings();
+    }
+}
---
+++ HalfboardStats/Application/StandingsFacade.cs
@@ -0,0 +1,23 @@
+﻿using HalfboardStats.Core.Builders;
+using HalfboardStats.Core.ObjectRelationalMappers.OrmInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Application
+{
+    public class StandingsFacade : IStandingsFacade
+    {
+        public IStandingsBuilder StandingsBuilder { get; set; }
+        public StandingsFacade(IStandingsBuilder builder)
+        {
+            StandingsBuilder = builder;
+        }
+        public async Task<IDictionary<string, IEnumerable<ITeamRecord>>> GetStandings()
+        {
+            var standings = await StandingsBuilder.BuildStandings();
+            return standings;
+        }
+    }
+}
