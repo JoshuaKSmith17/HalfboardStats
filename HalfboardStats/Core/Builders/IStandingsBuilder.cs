c66093fe57f0e1ab5d97b4acae9d0b094dcc0503
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-11-01 20:51:09 -0500

    Created interface for standingsbuilder to better serve dependency inversion and injection.

---
+++ HalfboardStats/Core/Builders/IStandingsBuilder.cs
@@ -0,0 +1,17 @@
+﻿using HalfboardStats.Core.JsonMappers.StandingsMappers;
+using HalfboardStats.Core.ObjectRelationalMappers.OrmInterfaces;
+using HalfboardStats.Infrastructure.ServiceAgents;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HalfboardStats.Core.Builders
+{
+    public interface IStandingsBuilder
+    {
+        IStandingsRepository Repository { get; set; }
+        IStandings Standings { get; set; }
+        IStandingsMapper StandingsMapper { get; set; }
+
+        Task<IDictionary<string, IEnumerable<ITeamRecord>>> BuildStandings();
+    }
+}
