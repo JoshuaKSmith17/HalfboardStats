189c1b0a6cbec7cc539bfdf12078e91a9c9cd928
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-11-03 21:17:50 -0500

    Removed dev buttons and functionality from the Index page.  These actions are now performed as a scheduled task.

--- HalfboardStats/Presentation/Pages/Index.cshtml.cs
+++ HalfboardStats/Presentation/Pages/Index.cshtml.cs
@@ -17,36 +17,11 @@ namespace HalfboardStats.Presentation.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        IServiceProvider ServiceProvider;
         public IDictionary<string, IEnumerable<ITeamRecord>> Standings { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger, IServiceProvider serviceProvider, HalfboardContext context)
         {
             _logger = logger;
-            ServiceProvider = serviceProvider;
-        }
-        public IActionResult OnPostBuildStats(IFormCollection data)
-        {
-            var controller = (IPlayerStatScraperController)ServiceProvider.GetService(typeof(IPlayerStatScraperController));
-            controller.ScrapePlayerStats();
-            return Page();
-        }
-
-        public IActionResult OnPostBuildPlayers(IFormCollection data)
-        {
-            var repo = (IActivePlayerLocalRepository)ServiceProvider.GetService(typeof(IActivePlayerLocalRepository));
-            repo.CreateAllPlayersAsync();
-            return Page();
-        }
-
-        public IActionResult OnPostBuildTeams(IFormCollection data)
-        {
-            var teamRepo = (ITeamLocalRepository)ServiceProvider.GetService(typeof(ITeamLocalRepository));
-            teamRepo.CreateTeams();
-            _logger.LogInformation("Teams Built");
-            return Page();
-
-
         }
     }
 }
