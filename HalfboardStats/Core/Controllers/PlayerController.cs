33b8dfb8998f9b845605970196a4486c32c857ff
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-11-06 13:41:19 -0600

    Added CreateAllPlayers method to Player controller.  This method iterates through each NHL season and sends a request to the PlayerAgent for player data for that particular season.

--- HalfboardStats/Core/Controllers/PlayerController.cs
+++ HalfboardStats/Core/Controllers/PlayerController.cs
@@ -13,12 +13,14 @@ namespace HalfboardStats.Core.Controllers
         public IPlayerRepository Repository { get; set; }
         public IPlayerbaseBuilder Builder { get; set; }
         public IPlayerAgent Agent { get; set; }
+        public ISeasonYear SeasonYear { get; set; }
 
-        public PlayerController(IPlayerRepository repository, IPlayerbaseBuilder builder, IPlayerAgent agent)
+        public PlayerController(IPlayerRepository repository, IPlayerbaseBuilder builder, IPlayerAgent agent, ISeasonYear seasonYear)
         {
             Repository = repository;
             Builder = builder;
             Agent = agent;
+            SeasonYear = seasonYear;
         }
 
         public async Task<List<Player>> GetActivePlayers()
@@ -31,6 +33,18 @@ namespace HalfboardStats.Core.Controllers
             List<RosterPersonMapper> rosterPersons = await Agent.GetActivePlayers();
             List<Player> players = Builder.Build(rosterPersons);
             await Repository.CreateOrUpdateAsync(players);
+        }
+
+        public async Task CreateAllPlayers()
+        {
+            while(SeasonYear.Year != SeasonYear.FirstYear)
+            {
+                List<RosterPersonMapper> rosterPersons = await Agent.GetAllPlayersAsync(SeasonYear.Year);
+                List<Player> players = Builder.Build(rosterPersons);
+                await Repository.CreateOrUpdateAsync(players);
+                SeasonYear.DecrementSeason();
+            }
+            
         }
     }
 }
