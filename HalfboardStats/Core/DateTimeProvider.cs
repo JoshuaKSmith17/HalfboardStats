1d966c82ff3d4dfbca81306693168ed1c3b35dcf
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-11-06 13:39:13 -0600

    Added DateTimeProvider.  Serves to devliver DateTime.Now information to the SeasonYear class in a way that allows for mockups.

---
+++ HalfboardStats/Core/DateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HalfboardStats.Core
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public int CurrentYear => DateTime.Now.Year;
+        public int CurrentMonth => DateTime.Now.Month;
+    }
+}
