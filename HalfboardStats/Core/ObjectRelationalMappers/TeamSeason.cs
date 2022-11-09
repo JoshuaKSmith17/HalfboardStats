0a21bc6631eca9ecc24859374107fe8efa89bf12
JoshuakSmith17 <joshuaksmith17@gmail.com>
2022-11-08 19:04:30 -0600

    Minor changes.

--- HalfboardStats/Core/ObjectRelationalMappers/TeamSeason.cs
+++ HalfboardStats/Core/ObjectRelationalMappers/TeamSeason.cs
@@ -8,7 +8,7 @@ namespace HalfboardStats.Core.ObjectRelationalMappers
     public class TeamSeason
     {
         /* 
-         * The goal of this class is to contain the data for the win loss record of a team and every other team stastic.
+         * The goal of this class is to contain the data for the win loss record of a team and every other team statistic.
          * The NHL API appears to have all this information obtainable from one get request of the teams/ID/stats endpoint.
          * This class should eventually make our Standings classes unneccessary.  We may choose to keep Standings for efficient
          * use of the smaller data set.
